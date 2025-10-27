e2d9a5f10a530d583d10281f349c11435e2d29eb
Winklerguido03 <Winklerguido2003@gmail.com>
2025-10-27 16:00:18 -0300

    actualizacion proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { FooterComponent } from "./components/footeer/footer.component";
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { PrincipalComponent } from "./components/principal/principal.component";
 import { TecnicoComponent } from "./components/tecnico/tecnico.component";
+import { ImagenesComponent } from "./components/imagenes/imagenes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SectionComponent, Section2Component, Section3Component, FooterComponent, RouterOutlet, RouterLink, ContactoComponent, PrincipalComponent, TecnicoComponent],
+  imports: [HeaderComponent, SectionComponent, Section2Component, Section3Component, FooterComponent, RouterOutlet, RouterLink, ContactoComponent, PrincipalComponent, TecnicoComponent, ImagenesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/imagenes/imagenes.component.ts
+++ src/app/components/imagenes/imagenes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Img2Component } from "../img2/img2.component";
 
 @Component({
   selector: 'app-imagenes',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './imagenes.component.html',
   styleUrl: './imagenes.component.css'
 })
